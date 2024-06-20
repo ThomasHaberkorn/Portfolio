3d4c5aeada616db823402aa5cf5c171e90b43c96
ThomasHaberkorn <126246580+ThomasHaberkorn@users.noreply.github.com>
2024-06-20 07:46:32 +0200

    contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarComponent } from './landing-page/navbar/navbar.component';
 import { AboutmeComponent } from './landing-page/aboutme/aboutme.component';
 import { MySkilsComponent } from './landing-page/my-skils/my-skils.component';
+import { PortfolioComponent } from './landing-page/portfolio/portfolio.component';
+import { ContactformComponent } from './landing-page/contactform/contactform.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutmeComponent, MySkilsComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent, AboutmeComponent, MySkilsComponent, PortfolioComponent, ContactformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
